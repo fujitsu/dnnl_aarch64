0122a8d6c53dbf8ffb1ebbab8133cec198b044c9
Kentaro KAWAKAMI <kawakami.k@fujitsu.com>
2020-01-29 18:29:00 +0900

    dnnl_aarch64:add native jit for reorder

--- src/cpu/cpu_isa_traits.hpp
+++ src/cpu/cpu_isa_traits.hpp
@@ -1,4 +1,20 @@
 /*******************************************************************************
+* Copyright 2019-2020 FUJITSU LIMITED
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/*******************************************************************************
 * Copyright 2018 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -49,6 +65,9 @@ typedef enum {
     avx512_mic,
     avx512_mic_4ops,
     avx512_core_bf16,
+    
+    simd,
+    sve,
 } cpu_isa_t;
 
 template <cpu_isa_t> struct cpu_isa_traits {}; /* ::vlen -> 32 (for avx2) */
@@ -85,6 +104,22 @@ template <> struct cpu_isa_traits<avx512_mic_4ops>:
 
 template <> struct cpu_isa_traits<avx512_core_bf16>:
     public cpu_isa_traits<avx512_common> {};
+
+template <>
+struct cpu_isa_traits<simd> {
+    typedef Xbyak::Xbyak_aarch64::VReg4S Vmm;
+    static constexpr int vlen_shift = 5;
+	static constexpr int vlen = 16;
+	static constexpr int n_vregs = 32;
+};
+
+template <> struct cpu_isa_traits<sve> {
+    typedef Xbyak::Xbyak_aarch64::ZRegS Vmm;
+	typedef Xbyak::Xbyak_aarch64::AdrScImm uni_ldst_addressing;
+	static constexpr int vlen_shift = 5;
+	static constexpr int vlen = 64;
+	static constexpr int n_vregs = 32;
+};
 
 namespace {
 
@@ -129,6 +164,12 @@ static inline bool mayiuse(const cpu_isa_t cpu_isa) {
         return true
             && mayiuse(avx512_core_vnni)
             && cpu.has(Cpu::tAVX512_BF16);
+    case simd: 
+        return true
+            && cpu.has(Cpu::tSIMD);
+    case sve:
+        return true
+            && cpu.has(Cpu::tSVE);
     case isa_any:
         return true;
     }
